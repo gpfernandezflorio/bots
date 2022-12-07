e03c9838f091d6f0dd3de5648629648545d0151e
Ralo <gpfernandezflorio@gmail.com>
2022-12-07 12:51:30 -0300

    Modifico imageDraw para que use cv2 en lugar de imagemagick

--- imageDraw.py
+++ imageDraw.py
@@ -1,74 +1,51 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
-# Requiere tener imageMagick instalado
-  # $ sudo apt install imagemagick
-
-import os
 from random import random, randint
+import cv2
 
 def abrir_imagen(ruta_imagen):
   return ImagenDibujable(ruta_imagen)
 
-def generar_color(color):
-  alfa = 1
-  if len(color) == 4:
-    alfa = color[3]
-  return "\"rgba(" + \
-    str(color[0]) + ", " + \
-    str(color[1]) + ", " + \
-    str(color[2]) + ", " + \
-    str(alfa) + ")\""
-
 class ImagenDibujable:
   def __init__(self, ruta):
-    self.ruta = ruta
-    self.dibujos = []
+    self.img = cv2.imread(ruta, 1)
     self.defaults = {
-      "font":"helvetica",
-      "pointsize":"20",
-      "fill":"\"rgba(0, 0, 0, 1)\""
+      "font":cv2.FONT_HERSHEY_COMPLEX,
+      "pointsize":20,
+      "fill":(0, 0, 0)
     }
 
+  # cv2.FONT_ ...
   def set_fuente(self, font):
-    this.defaults["font"] = font
+    self.defaults["font"] = font
 
+  # número
   def set_tamaño(self, size):
-    this.defaults["pointsize"] = str(size)
+    self.defaults["pointsize"] = size
 
+  # (número, número, número)
   def set_color(self, color):
-    this.defaults["fill"] = generar_color(color)
+    self.defaults["fill"] = color
 
+  # texto : string
+  # posicion : (número, número)
+  # color : (número, número, número)
+  # tamaño : número
+  # fuente : cv2.FONT_ ...
   def escribir(self, texto, posicion, color=None, tamaño=None, fuente=None):
-    nuevo_dibujo = "-font "
-    if (fuente is None):
-      nuevo_dibujo += self.defaults["font"]
-    else:
-      nuevo_dibujo += fuente
-    nuevo_dibujo += " -pointsize "
-    if (tamaño is None):
-      nuevo_dibujo += self.defaults["pointsize"]
-    else:
-      nuevo_dibujo += str(tamaño)
-    nuevo_dibujo += " -fill "
-    if (color is None):
-      nuevo_dibujo += self.defaults["fill"]
-    else:
-      nuevo_dibujo += generar_color(color)
-    nuevo_dibujo += " -draw \"text " + \
-      str(posicion[0]) + "," + str(posicion[1]) + \
-      "'" + texto + "'\" "
-    self.dibujos.append(nuevo_dibujo)
+    if color is None:
+      color = self.defaults["fill"]
+    if tamaño is None:
+      tamaño = self.defaults["pointsize"]
+    if fuente is None:
+      fuente = self.defaults["font"]
+    cv2.putText(self.img,texto,posicion,fuente,tamaño/30.0,color,2)
 
   def guardar_imagen(self, ruta):
-    comando = "convert " + self.ruta + " "
-    for dibujo in self.dibujos:
-      comando += dibujo
-    comando += ruta
     try:
-        os.system(comando)
+        cv2.imwrite(ruta, self.img)
     except:
-        print("Error al ejecutar")
+        print("Error al guardar imagen")
 
 def random_text(filein, text, imgsize, n, fileout):
   imagen = abrir_imagen(filein)
@@ -76,8 +53,7 @@ def random_text(filein, text, imgsize, n, fileout):
   h = imgsize[1]
   for i in range(n):
     imagen.escribir(text,
-      [randint(int(-w/120),int(2*w/3)),randint(int(h/6),int(5*h/6))],
+      (randint(int(-w/120),int(2*w/3)),randint(int(h/6),int(5*h/6))),
       tamaño=randint(int(w/60),int(w/10)),
-      color=[randint(50,250),randint(50,250),randint(50,250),0.4+random()/2],
-      fuente="impact")
+      color=(randint(50,250),randint(50,250),randint(50,250),0.4+random()/2))
   imagen.guardar_imagen(fileout)
